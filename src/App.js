946487b16d094e53d7d0748cd4c2f8e6a18f7775
Amanullah Aman <amancse.aiub@gmail.com>
2023-07-13 13:00:20 +0600

    meals list added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 
 import Header from "./components/Layouts/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Meals />
     </Fragment>
   );
 }
