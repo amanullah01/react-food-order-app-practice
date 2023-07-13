c234c8ce89cd254a9e61f99996f0b73ecbf5cc9f
Amanullah Aman <amancse.aiub@gmail.com>
2023-07-13 13:29:41 +0600

    Individual meal items are showing

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -29,12 +31,23 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealList = DUMMY_MEALS.map((meal) => {
-    return <li>{meal.name}</li>;
+    return (
+      <li>
+        <MealItem
+          key={meal.id}
+          name={meal.name}
+          description={meal.description}
+          price={meal.price}
+        />
+      </li>
+    );
   });
 
   return (
     <section className={classes.meals}>
-      <ul>{mealList}</ul>
+      <ul>
+        <Card>{mealList}</Card>
+      </ul>
     </section>
   );
 };
