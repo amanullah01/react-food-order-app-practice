0d418935da5a5074d073a9060283c9d724a6a50e
Amanullah Aman <amancse.aiub@gmail.com>
2023-08-13 12:56:44 +0600

    reading value from checkout form

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,31 +1,43 @@
+import { useRef } from "react";
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const cityInputRef = useRef();
+  const postalInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+    const name = nameInputRef.current.value;
+    const street = streetInputRef.current.value;
+    const city = cityInputRef.current.value;
+    const postal = postalInputRef.current.value;
   };
   return (
-    <form onSubmit={confirmHandler}>
+    <form onSubmit={confirmHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor="street">Stree</label>
-        <input type="text" id="street" />
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="posta" />
+        <input type="text" id="postal" ref={postalInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Close
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
