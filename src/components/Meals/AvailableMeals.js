0eb7c1f9e5d096073a07fed7fcbfa9b4fe71345d
Amanullah Aman <amancse.aiub@gmail.com>
2023-08-13 11:32:54 +0600

    Add loading effect

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
@@ -24,10 +26,19 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p className={classes.MealLoading}>Loading</p>
+      </section>
+    );
+  }
 
   const mealList = meals.map((meal) => {
     return (
